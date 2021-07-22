8c562ae5b2dd5708d4ab3d5a6c4938f91fd5a42b
tatsuo48 <green.americanidiot.yokoyama@gmail.com>
2021-07-22 13:56:04 +0900

    add deploy script
    デプロイ時に行う操作をまとめたスクリプトを作った。

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/isucon/isutrain
+export LANGUAGE=go
+# デプロイ作業
+docker-compose -f webapp/docker-compose.yml -f webapp/docker-compose.${LANGUAGE}.yml stop
+docker-compose -f webapp/docker-compose.yml -f webapp/docker-compose.${LANGUAGE}.yml build
+docker-compose -f webapp/docker-compose.yml -f webapp/docker-compose.${LANGUAGE}.yml up
+
+# MySQLインデックス貼り
+mysql -h 0.0.0.0 -P 13306 -ppassword < webapp/sql/index.sql
