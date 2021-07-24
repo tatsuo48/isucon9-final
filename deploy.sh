5a26bd5617a6eff0305ec5eded29dbaf3b774ca6
tatsuo48 <green.americanidiot.yokoyama@gmail.com>
2021-07-24 21:37:24 +0900

    add_Sleep
    コンテナ起動前にクエリを投げて失敗してしまったので10秒待ってからインデックス貼るようにする

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,5 @@ docker-compose -f webapp/docker-compose.yml -f webapp/docker-compose.${LANGUAGE}
 docker-compose -f webapp/docker-compose.yml -f webapp/docker-compose.${LANGUAGE}.yml up -d
 
 # MySQLインデックス貼り
+sleep 10
 mysql -h 0.0.0.0 -P 13306 -ppassword < webapp/sql/100_index.sql
